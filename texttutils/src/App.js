1019980164f0a60b3053226e58735628993eb301
mandeep thakur <tmanu76269@gmail.com>
2022-03-16 11:21:06 +0530

    HEAD

---
+++ texttutils/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+let name = "mandeep thakur";
+
+function App() {
+  return (
+    <>
+
+
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ex ipsa rem maiores sit repellat cum quibusdam dolorum est esse, natus cumque veniam consequuntur placeat, obcaecati corporis odio facilis exercitationem Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur harum quos voluptatem nemo dolorem rerum fugiat placeat vel aspernatur consectetur similique iste maiores deserunt eveniet consequatur, dolore optio! Ipsa, quo</p>            
+      </div>
+<div className="blank"></div>
+</>
+  );
+}
+export default App;
